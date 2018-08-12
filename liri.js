01a6d705adc54624014e26ec2828b6ae886f304b
Justin Riddle <justinriddle@Justins-iMac.local>
2018-08-12 11:03:34 -0700

    updated files

--- liri.js
+++ liri.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
-var request = require("request");
 
 // Add the code required to import the keys.js file and store it in a variable. ??
 
-/* 
+var keys = require('./keys.js');
+var request = require('request');
+var twitter = require('twitter');
+var spotify = require('spotify');
+var client = new twitter(keys.twitterKeys);
+var fs = require('fs');
+
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter); 
-*/
 
 var command = process.argv[2];
 var name = process.argv[3];
@@ -21,10 +26,10 @@ var name = process.argv[3];
 ??????
 */
 
-if(command == 'movie-this') {
-    console.log(command);
-    moviethis(name);
-}
+// if(command == 'movie-this') {
+//     console.log(command);
+//     moviethis(name);
+// }
 
 /* YOU CAN FORMAT THE ABOVE CODE AS BELOW, IF AND ONLY IF THE FUNCTION ONLY DOES ONE THING:
 
@@ -32,24 +37,145 @@ if(command == 'movie-this') moviethis();
 
 */
 
+switch(command){
+  case "my-tweets":
+    twitter();
+  break;
 
-function mytweets() {
-  // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+  case "spotify-this-song":
+    if(x){
+      spotify(x);
+    } else{
+      spotify("Dark Side of the Moon");
+    }
+  break;
+
+  case "movie-this":
+    if(x){
+      omdb(x)
+    } else{
+      omdb("Mr. Nobody")
+    }
+  break;
+
+  case "do-what-it-says":
+    doThing();
+  break;
+
+  default:
+    console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+  break;
 }
 
-function spotifythissong(song_name) {
-  /* This will show the following information about the song in your terminal/bash window
-    Artist(s)
-    The song's name
-    A preview link of the song from Spotify
-    The album that the song is from 
-    
-    If no song is provided then your program will default to "The Sign" by Ace of Base.
-    
-    */
+function twitter(){
+  var screenName = {screen_name: 'ProblyBrilliant'};
+  client.get('statuses/user_timeline', screenName, function(error, tweets, response){
+    if(!error){
+      for(var i = 0; i<tweets.length; i++){
+        var date = tweets[i].created_at;
+        console.log("@ProblyBrilliant: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        console.log("-----------------------");
+        
+        fs.appendFile('log.txt', "@ProblyBrilliant: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        fs.appendFile('log.txt', "-----------------------");
+      }
+    }else{
+      console.log('Error occurred');
+    }
+  });
 }
 
-function moviethis(movie_name) {
+function spotify(song){
+  spotify.search({ type: 'track', query: song}, function(error, data){
+    if(!error){
+      for(var i = 0; i < data.tracks.items.length; i++){
+        var songData = data.tracks.items[i];
+        console.log("Artist: " + songData.artists[0].name);
+        console.log("Song: " + songData.name);
+        console.log("Preview URL: " + songData.preview_url);
+        console.log("Album: " + songData.album.name);
+        console.log("-----------------------");
+        
+        fs.appendFile('log.txt', songData.artists[0].name);
+        fs.appendFile('log.txt', songData.name);
+        fs.appendFile('log.txt', songData.preview_url);
+        fs.appendFile('log.txt', songData.album.name);
+        fs.appendFile('log.txt', "-----------------------");
+      }
+    } else{
+      console.log('Error occurred.');
+    }
+  });
+}
+
+function omdb(movie){
+  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true';
+
+  request(omdbURL, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+      fs.appendFile('log.txt', "Title: " + body.Title);
+      fs.appendFile('log.txt', "Release Year: " + body.Year);
+      fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
+      fs.appendFile('log.txt', "Country: " + body.Country);
+      fs.appendFile('log.txt', "Language: " + body.Language);
+      fs.appendFile('log.txt', "Plot: " + body.Plot);
+      fs.appendFile('log.txt', "Actors: " + body.Actors);
+      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
+      fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
+
+    } else{
+      console.log('Error occurred.')
+    }
+    if(movie === "Mr. Nobody"){
+      console.log("-----------------------");
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!");
+
+      fs.appendFile('log.txt', "-----------------------");
+      fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      fs.appendFile('log.txt', "It's on Netflix!");
+    }
+  });
+
+}
+
+function doThing(){
+  fs.readFile('random.txt', "utf8", function(error, data){
+    var txt = data.split(',');
+
+    spotifySong(txt[1]);
+  });
+}
+
+// function mytweets() {
+//   // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+// }
+
+// function spotifythissong(song_name) {
+//   /* This will show the following information about the song in your terminal/bash window
+//     Artist(s)
+//     The song's name
+//     A preview link of the song from Spotify
+//     The album that the song is from 
+    
+//     If no song is provided then your program will default to "The Sign" by Ace of Base.
+    
+//     */
+// }
+
+// function moviethis(movie_name) {
     /*
     This will output the following information to your terminal/bash window:
 
@@ -75,6 +201,8 @@ It's on Netflix!
 
 // the OMDB API requires an API key. You may use trilogy
 
+/*
+
 request("http://www.omdbapi.com/?t=" + movie_name + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
   // If the request is successful (i.e. if the response status code is 200)
@@ -90,8 +218,9 @@ request("http://www.omdbapi.com/?t=" + movie_name + "&y=&plot=short&apikey=trilo
 function dowhatitsays() {
     /* Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
-
 It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-Feel free to change the text in that document to test out the feature for other commands. */
+Feel free to change the text in that document to test out the feature for other commands.
 
 }
+
+*/
